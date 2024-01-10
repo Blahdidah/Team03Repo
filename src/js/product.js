160aa98e024d2f491aa28c366273deca1424db97
Blahdidah <80852722+Blahdidah@users.noreply.github.com>
2024-01-10 12:12:20 -0700

    Individual Cart Fix
    Changes for individual Assignment

--- src/js/product.js
+++ src/js/product.js
@@ -1,10 +1,14 @@
-import { setLocalStorage } from "./utils.mjs";
+import { setLocalStorage, getLocalStorage } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
 
 const dataSource = new ProductData("tents");
 
 function addProductToCart(product) {
-  setLocalStorage("so-cart", product);
+  const existingCart = getLocalStorage("so-cart") || [];
+  existingCart.push(product);
+
+  //setLocalStorage('so-cart', product);
+  setLocalStorage("so-cart", existingCart);
 }
 // add to cart button event handler
 async function addToCartHandler(e) {
