360d997085418fcd32345bd842611ad422fdec41
Lenora Stevens <114180320+lenorastevens@users.noreply.github.com>
2024-01-16 15:04:11 -0600

    team assignment 2

--- src/js/product.js
+++ src/js/product.js
@@ -1,25 +1,8 @@
-import { setLocalStorage, getLocalStorage } from './utils.mjs';
+import { getParam } from './utils.mjs';
 import ProductData from './ProductData.mjs';
+import ProductDetails from './ProductDetails.mjs';
 
 const dataSource = new ProductData('tents');
-
-function addProductToCart(product) {
-  //construct an existing cart, check if it is an array
-  let existingCart = getLocalStorage('so-cart') || [];
-  if (!Array.isArray(existingCart)) {
-    existingCart = [];
-  }
-  existingCart.push(product);
-
-  setLocalStorage('so-cart', existingCart);
-}
-// add to cart button event handler
-async function addToCartHandler(e) {
-  const product = await dataSource.findProductById(e.target.dataset.id);
-  addProductToCart(product);
-}
-
-// add listener to Add to Cart button
-document
-  .getElementById('addToCart')
-  .addEventListener('click', addToCartHandler);
+const productId = getParam('product');
+const product = new ProductDetails(productId, dataSource)
+product.init();
