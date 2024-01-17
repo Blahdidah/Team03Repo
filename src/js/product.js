9c54d3e9d814f7ba12ab15825fb0a6b98794cbea
Blahdidah <80852722+Blahdidah@users.noreply.github.com>
2024-01-17 15:49:00 -0700

    total$ in cart
    created HTML code for the total in cart, also added JS for the cart, checking if it's empty or not, as well as calculating total.

--- src/js/product.js
+++ src/js/product.js
@@ -4,5 +4,5 @@ import ProductDetails from './ProductDetails.mjs';
 
 const dataSource = new ProductData('tents');
 const productId = getParam('product');
-const product = new ProductDetails(productId, dataSource)
+const product = new ProductDetails(productId, dataSource);
 product.init();
