329f7d64f923de0adae96bba96cbe83857a28087
crewsbd <bdcrews@protonmail.com>
2024-01-16 18:33:47 -0800

    Fixed product_pages query to match team after pull.

--- src/js/ProductListing.mjs
+++ src/js/ProductListing.mjs
@@ -9,7 +9,7 @@ import { renderListWithTemplate } from './utils.mjs';
 
 function productCardTemplate(product) {
   return `<li class="product-card">
-        <a href="/product_pages/?Id=${product.Id}">
+        <a href="/product_pages/?product=${product.Id}">
             <img src="${product.Image}" alt="${product.Name}">
             <h3 class="card__brand">${product.Brand.Name}</h3>
             <h2 class="card__name">${product.Name}</h2>
