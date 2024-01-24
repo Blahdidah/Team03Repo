744d1bdda4607bb0a1dd7d5ffbfd7fcb231e45fa
crewsbd <bdcrews@protonmail.com>
2024-01-24 10:00:39 -0800

    Moved cart.js contents to ShoppingCart.mjs

---
+++ src/js/product-listing.js
@@ -0,0 +1,19 @@
+// Pairs to /index.html
+
+import {loadHeaderFooter} from './utils.mjs';
+import ProductData from './ProductData.mjs';
+import ProductListing from './ProductListing.mjs';
+
+
+let tents = new ProductData('tents'); // All the tents
+let listing = new ProductListing(
+  'tents',
+  tents,
+  document.querySelector('.product-list')
+); // Listing for the tents
+
+listing.init(); // Renders the listing to the page
+
+loadHeaderFooter('partials');
+//updateCartCountIcon(document.querySelector('.cart'));
+
