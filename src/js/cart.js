7c87d73455208bdd1b3ac6b5c235a5191bb29019
Blahdidah <80852722+Blahdidah@users.noreply.github.com>
2024-01-26 19:22:52 -0700

    Update cart.js
    Update to the cart.js to match this week's individual assignment.

--- src/js/cart.js
+++ src/js/cart.js
@@ -1,6 +1,10 @@
 import {loadHeaderFooter} from './utils.mjs';
 import ShoppingCart from './ShoppingCart.mjs';
 
+const shoppingCart = new ShoppingCart();
+shoppingCart.renderCartContents();
+loadHeaderFooter('partials');
+
 document.addEventListener('click', function (event) {
   if (event.target.classList.contains('remove-item')) {
     const itemId = event.target.getAttribute('data-id');
