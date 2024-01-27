164a29297b49d4cae2de520be1fa15392e68ba02
jessochs <fac17002@byui.edu>
2024-01-26 18:32:11 -0700

    jf branch

--- src/js/product-listing.js
+++ src/js/product-listing.js
@@ -4,19 +4,25 @@ import {loadHeaderFooter, getParam } from './utils.mjs';
 import ProductData from './ProductData.mjs';
 import ProductListing from './ProductListing.mjs';
 
-loadHeaderFooter('partials');
+loadHeaderFooter();
 
 
 const category = getParam('category');
 
 const productData = new ProductData(); // All the tents
-const listing = new ProductListing(
-  category,
-  productData,
-  document.querySelector('.product-list')
-); // Listing for the tents
+const element = document.querySelector('product-list');
+
+const listing = new ProductListing(category, productData, element);
+
+
+// const listing = new ProductListing(
+//   category,
+//   productData,
+//   document.querySelector('.product-list')
+// ); // Listing for the tents
 
 listing.init(); // Renders the listing to the page
+
 
 
 //updateCartCountIcon(document.querySelector('.cart'));
